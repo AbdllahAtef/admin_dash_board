895603b9b2615fcbfafa93efbf05a3822c7bdeee
AbdllahAtef <abdllahatef7@gmail.com>
2026-05-15 22:28:27 +0300

    Update BASE_URL to include '/api' endpoint

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://smartuniversity.runasp.net/";
+const BASE_URL = "http://smartuniversity.runasp.net/api";
 
 async function loginUser(email, password) {
     const response = await fetch(`${BASE_URL}/auth/login`, {
